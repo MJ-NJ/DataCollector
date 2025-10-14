aeeeefb6194a0918545d857ad3d00c2429d6d7dd
MJ-NJ <mjha_be21@thapar.edu>
2025-10-14 15:31:08 +0530

    Added YAML parser

--- cmd/main.go
+++ cmd/main.go
@@ -20,23 +20,44 @@ func main() {
 			&cli.StringFlag{
 				Name:     "path",
 				Usage:    "provide the file path of the input here",
-				Required: true,
+				Required: false,
 			},
 			&cli.BoolFlag{
 				Name:  "dry-run",
 				Usage: "use this when need to print the site",
 			},
+			&cli.BoolFlag{
+				Name:  "easter-egg",
+				Usage: "this is an easter egg",
+			},
 		},
 		Action: func(ctx *cli.Context) error {
+
+			if ctx.Bool("easter-egg") {
+				fmt.Println("This is a easter egg")
+				return nil
+			}
+
 			configPath := ctx.String("path")
-			config, err := config.LoadConfig(configPath)
-			if err != nil {
-				return err
+			var conf *config.ScrapeConfig
+			var err error
+
+			if configPath == "" {
+				conf, err = config.LoadConfig("config.json") // This is the default val if string not present
+				if err != nil {
+					return err
+				}
+			} else {
+				conf, err = config.LoadConfig(configPath) // Now this will take any path needed
+				if err != nil {
+					return err
+				}
 			}
 
 			if ctx.Bool("dry-run") {
-				fmt.Printf("[Dry Run] Would scrape the site: %s", config.URL)
+				fmt.Printf("[Dry Run] Would scrape the site: %s", conf.URL)
 			}
+
 			fmt.Printf("\nstarted scraping")
 			return nil
 		},
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/MJ-NJ/DataCollector
 
 go 1.25.2
 
-require github.com/go-playground/validator/v10 v10.28.0
+require (
+	github.com/go-playground/validator/v10 v10.28.0
+	github.com/urfave/cli/v2 v2.27.7
+)
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
@@ -11,7 +14,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.27.7 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
